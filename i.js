f4a6496e029e520728a2f5398756605f6dbe4837
RanveerArnavkamboj <ranbeerkamboj45@gmail.com>
2026-03-12 21:05:06 +0530

    Update i.js

--- i.js
+++ i.js
@@ -1,10 +1,34 @@
-let newgame=document.querySelector(".newgame")
+    <script>
+let options=document.querySelector(".options")
+let btnX=document.querySelector(".X")
+let btnO=document.querySelector(".O")
+let scorep0=0;
+let scorep=0;
+    let p0=document.querySelector(".p0")
+    let p=document.querySelector(".p×")
+      let newgame=document.querySelector(".newgame")
 let con=document.querySelector(".con")
 let main=document.querySelector("main")
 let btns=document.querySelectorAll(".btn")
 let res=document.querySelector(".res")
-let resetbtn=document.querySelector(".reset")
-let turn0=true
+let resetbtn=document.querySelector(".reset") 
+main.classList.add("maincss")
+let turn0=""
+btnX.addEventListener("click",()=>{
+ turn0=false 
+  main.classList.remove("maincss")
+  options.classList.add("optionsafter")
+  options.classList.remove("options")
+ return turn0
+
+})
+btnO.addEventListener("click",()=>{
+  turn0=true
+main.classList.remove("maincss")
+options.classList.add("optionsafter")
+  options.classList.remove("options")
+  return turn0
+})
 let wp=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 resetbtn.addEventListener("click",()=>{
   abled()
@@ -12,7 +36,8 @@ resetbtn.addEventListener("click",()=>{
 newgame.addEventListener("click",()=>{
   abled()
   con.classList.add("connone")
-  main.classList.remove("maincss")
+  options.classList.remove("optionsafter")
+  options.classList.add("options")
 })
 btns.forEach(box =>{
   box.addEventListener("click",()=>{
@@ -29,7 +54,7 @@ btns.forEach(box =>{
     patterncheck()
   })
 })
-function patterncheck(){
+function patterncheck(){ 
 for (let pattern of wp){
 let place1=btns[pattern[0]]
 let place2=btns[pattern[1]]
@@ -37,6 +62,12 @@ let place3=btns[pattern[2]]
 if (place1.innerText!="" && place2.innerText!="" && place3.innerText!=""){
 if (place1.innerText==place2.innerText&&place2.innerText==place3.innerText){
   main.classList.add("maincss")
+  if (place1.innerText=="0"){
+   p0.textContent=scorep0+=1
+  }
+  if (place1.innerText=="\u00D7"){
+   p.textContent=scorep+=1
+  }
   res.textContent=`Congratulations \u{1F389} player ${place1.textContent} is winner`
   res.classList.add("rescss")
   con.classList.remove("connone")
@@ -55,4 +86,11 @@ function  abled(){
     box.disabled=false
     box.textContent=""
   })
-    }
+}
+    </script>
+
+
+
+
+
+    
